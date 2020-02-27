087b866ad6b258e2a25e22ca9cf2c68c91272724
steveniiv <291766389@qq.com>
2020-02-27 13:53:33 +0800

    feat(): 新增comment功能.3

--- app/javascripts/test.js
+++ app/javascripts/test.js
@@ -13,35 +13,57 @@ window.App = {
         });
     },
     addComment: function () {
+        var type = document.getElementById("select").value;
         var objectId = document.getElementById("objectId").value;
         var rating = document.getElementById("rating").value;
         var comment = document.getElementById("comment").value;
         Comment.deployed().then(function (instance) {
-            return instance.addArticleComment(objectId, rating, comment,{from:App.account,gas:500000});
+            if (type == 1){
+                return instance.addObjectComment(objectId, rating, comment,{from:App.account,gas:500000});
+            } else if (type == 0){
+                return instance.addArticleComment(objectId, rating, comment,{from:App.account,gas:500000});
+            }
         }).then(function (res) {
             console.log(res);
         }).catch(function (err) {
             console.log(err);
-        })
+        });
     },
     getLength: function() {
+        var type = document.getElementById("select").value;
         Comment.deployed().then(function (instance) {
-            return instance.getArticleCommentsLength.call(1)
+            if (type == 1){
+                return instance.getObjectCommentsLength.call(1)
+            } else if (type == 0){
+                return instance.getArticleCommentsLength.call(1)
+            }
         }).then(function (res) {
             document.getElementById("show").innerText = res;
             return res;
         })
     },
     getComment: function () {
-        var articleId = document.getElementById("queryArticleId").value;
+        document.getElementById("articlesRow").innerHTML = "";
+        var type = document.getElementById("select").value;
+        var id = document.getElementById("queryArticleId").value;
         Comment.deployed().then(function (instance) {
-            return instance.getArticleCommentsLength.call(articleId);
+            if (type == 1){
+                return instance.getObjectCommentsLength.call(id);
+            } else if (type == 0){
+                return instance.getArticleCommentsLength.call(id);
+            }
         }).then(function (size) {
             Comment.deployed().then(function (instance) {
                 for (var i=0;i<size;i++){
-                    instance.getArticleComment(articleId,i).then(function (article) {
-                        App.displayComment(article[0],article[1],article[2],article[3]);
-                    })
+                    if (type == 1){
+                        instance.getObjectComment(id,i).then(function (object) {
+                            App.displayComment(object[0],object[1],object[2],object[3]);
+                        })
+                    } else if(type == 0){
+                        instance.getArticleComment(id,i).then(function (article) {
+                            App.displayComment(article[0],article[1],article[2],article[3]);
+                        })
+                    }
                 }
             });
         })
